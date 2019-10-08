ff0304ab95d0a82fc53b0d1d14603dcd653e5af9
Rudolph Almeida <rudolf1.almeida@gmail.com>
2019-10-08 13:49:07 +1100

    Window resizing and canvas size

--- client/src/main/java/com/framelessboard/App.java
+++ client/src/main/java/com/framelessboard/App.java
@@ -19,6 +19,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("draw"));
         stage.setTitle("FramelessBoard");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }

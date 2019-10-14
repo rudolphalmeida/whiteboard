a94ff1cb00ed663a547161e2fb9dc2020f23217b
Reyham4 <55683760+Reyham4@users.noreply.github.com>
2019-10-14 13:45:45 +1100

    CustomDrawing class
    To be used to encapsulate necessary data such as startX or startY for drawing on other clients.

---
+++ client/src/main/java/com/framelessboard/CustomDrawing.java
@@ -0,0 +1,32 @@
+import java.awt.geom.*;
+import java.io.Serializable;
+
+public class CustomDrawing implements Serializable{
+    
+    Object drawing;
+    
+    CustomDrawing(Object drawing) {
+        //if (drawing instanceof ArrayList) {
+            this.drawing = drawing;
+        //}
+        /*
+        else if (drawing instanceof Line2D) {
+            
+        }
+        else if (drawing instanceof Rectangle2D) {
+            
+        }
+        else if (drawing instanceof Ellipse2D) {
+            
+        }
+        */
+    }
+    /*
+    public Object get() {
+        if (data instanceof ArrayList) {
+            return 
+        }
+    }
+    */
+    
+}
